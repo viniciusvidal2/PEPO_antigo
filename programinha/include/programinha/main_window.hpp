c48ce73dc4e643d213be9a9633b3587571b95b32
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2020-06-16 09:31:21 -0300

    comandos indo por ssh

---
+++ programinha/include/programinha/main_window.hpp
@@ -0,0 +1,64 @@
+/**
+ * @file /include/programinha/main_window.hpp
+ *
+ * @brief Qt based gui for programinha.
+ *
+ * @date November 2010
+ **/
+#ifndef programinha_MAIN_WINDOW_H
+#define programinha_MAIN_WINDOW_H
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui/QMainWindow>
+#include "ui_main_window.h"
+#include "qnode.hpp"
+#include <libssh/libssh.h>
+
+/*****************************************************************************
+** Namespace
+*****************************************************************************/
+
+namespace programinha {
+
+using namespace std;
+
+/*****************************************************************************
+** Interface [MainWindow]
+*****************************************************************************/
+/**
+ * @brief Qt central, all operations relating to the view part here.
+ */
+class MainWindow : public QMainWindow {
+Q_OBJECT
+
+public:
+	MainWindow(int argc, char** argv, QWidget *parent = 0);
+	~MainWindow();
+
+        void closeEvent(QCloseEvent *event); // Overloaded function
+
+public Q_SLOTS:
+
+
+private Q_SLOTS:
+
+        void on_pushButton_iniciarcaptura_clicked();
+        void on_pushButton_finalizarcaptura_clicked();
+        void on_pushButton_cameraimagemcalibrar_clicked();
+
+        void on_horizontalSlider_brightness_sliderReleased();
+        void on_horizontalSlider_exposure_sliderReleased();
+
+private:
+	Ui::MainWindowDesign ui;
+	QNode qnode;
+        ssh_session pepo_ssh;
+        int pepo_ssh_port;
+};
+
+}  // namespace programinha
+
+#endif // programinha_MAIN_WINDOW_H
---
+++ programinha/src/main_window.cpp
@@ -0,0 +1,144 @@
+/**
+ * @file /src/main_window.cpp
+ *
+ * @brief Implementation for the qt gui.
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui>
+#include <QMessageBox>
+#include <iostream>
+#include "../include/programinha/main_window.hpp"
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace programinha {
+
+using namespace Qt;
+
+/*****************************************************************************
+** Implementation [MainWindow]
+*****************************************************************************/
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MainWindow::MainWindow(int argc, char** argv, QWidget *parent)
+    : QMainWindow(parent)
+    , qnode(argc,argv)
+{
+    ui.setupUi(this); // Calling this incidentally connects all ui's triggers to on_...() callbacks in this class.
+    QObject::connect(ui.actionAbout_Qt, SIGNAL(triggered(bool)), qApp, SLOT(aboutQt())); // qApp is a global variable for the application
+
+    setWindowIcon(QIcon(":/images/icon.png"));
+    //	ui.tab_manager->setCurrentIndex(0); // ensure the first tab is showing - qt-designer should have this already hardwired, but often loses it (settings?).
+    QObject::connect(&qnode, SIGNAL(rosShutdown()), this, SLOT(close()));
+
+    // Acertando o SSH
+    pepo_ssh = ssh_new();
+    int verbosity = SSH_LOG_WARNING;
+    ssh_options_set(pepo_ssh, SSH_OPTIONS_HOST, "192.168.0.101");
+    ssh_options_set(pepo_ssh, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
+    ssh_options_set(pepo_ssh, SSH_OPTIONS_PORT, &pepo_ssh_port);
+    ssh_options_set(pepo_ssh, SSH_OPTIONS_USER, "pepo");
+    int rc = ssh_connect(pepo_ssh);
+    if(rc == SSH_OK)
+        ROS_INFO("Conectamos por SSH.");
+    else
+        ROS_ERROR("Nao foi possivel conectar por SSH.");
+    string password = "12";
+    rc = ssh_userauth_password(pepo_ssh, "pepo", password.c_str());
+    if(rc == SSH_AUTH_SUCCESS)
+        ROS_INFO("Conectamos por SSH com o password.");
+    else
+        ROS_ERROR("Nao foi possivel conectar por SSH com o password.");
+
+    // Acertando os limites dos sliders
+    ui.horizontalSlider_exposure->setRange(300, 2200);
+    ui.horizontalSlider_brightness->setRange(50, 130);
+    ui.horizontalSlider_exposure->setValue(1500);
+    ui.horizontalSlider_brightness->setValue(80);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MainWindow::~MainWindow() {}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    ssh_disconnect(pepo_ssh);
+    ssh_free(pepo_ssh);
+    QMainWindow::closeEvent(event);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}  // namespace programinha
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void programinha::MainWindow::on_pushButton_iniciarcaptura_clicked()
+{
+    // Nome da pasta para gravar os dados
+    string pasta = ui.lineEdit_pasta->text().toStdString();
+    // Montando comando de acordo com a escolha nos radiobuttons
+    string comando;
+    if(ui.radioButton_space->isChecked())
+        comando = "roslaunch pepo_space pepo_space.launch pasta:="+pasta;
+    else if(ui.radioButton_object->isChecked())
+        comando = "roslaunch pepo_obj pepo_obj.launch pasta:="+pasta;
+
+    // Iniciando canal e secao SSH
+    ssh_channel channel;
+    channel = ssh_channel_new(pepo_ssh);
+    char buffer[256];
+    int nbytes;
+    if(ssh_channel_open_session(channel) == SSH_OK){
+        if(ssh_channel_request_exec(channel, comando.c_str()) == SSH_OK)
+            ROS_INFO("Comando de inicio do processo foi enviado.");
+        else
+            ROS_ERROR("Comando de inicio da captura nao pode ser executado.");
+        // Acertando a camera de acordo com os sliders
+        ssh_channel_request_exec(channel, "comando v4...");
+        ssh_channel_request_exec(channel, "comando v4...");
+        // Falando o que tem de output do roslaunch
+        char buffer[256];
+        int nbytes;
+        nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+        while (nbytes > 0)
+        {
+          if (fwrite(buffer, 1, nbytes, stdout) != nbytes)
+          {
+            ssh_channel_close(channel);
+            ssh_channel_free(channel);
+            cout<< SSH_ERROR;
+          }
+          nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+        }
+
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void programinha::MainWindow::on_pushButton_finalizarcaptura_clicked(){
+    // Iniciando canal e secao SSH
+    ssh_channel channel;
+    channel = ssh_channel_new(pepo_ssh);
+    char buffer[256];
+    int nbytes;
+    if(ssh_channel_open_session(channel) == SSH_OK){
+        // Enviando comando para matar os nos
+        if(ssh_channel_request_exec(channel, "rosnode kill --all") == SSH_OK)
+            ROS_INFO("Comando de matar todos os nos de ROS foi enviado.");
+        else
+            ROS_ERROR("Comando de matar todos os nos de ROS nao pode ser executado.");
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void programinha::MainWindow::on_horizontalSlider_brightness_sliderReleased(){
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void programinha::MainWindow::on_horizontalSlider_exposure_sliderReleased(){
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void programinha::MainWindow::on_pushButton_cameraimagemcalibrar_clicked(){
+
+}
